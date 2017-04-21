8ae1abd4d7913ca8d9e3ce76728552b5d17d454a
Kyle Buscaglia <kyle.buscaglia@gmail.com>
2017-04-21 17:28:28 -0400

    Init controller

--- ElevatorSim/src/ElevatorController.java
+++ ElevatorSim/src/ElevatorController.java
@@ -6,7 +6,31 @@ public class ElevatorController {
 	private ArrayList<Elevator> elevators; 
 	
 	public ElevatorController(int floors , int numElevators){
+		this.floors = floors;
 		
 	}
+	
+	private void createElevators(int numElev){
+		for(int i=0; i<numElev; i++){
+			
+		}
+	}
+
+	public int getFloors() {
+		return floors;
+	}
+
+	public void setFloors(int floors) {
+		this.floors = floors;
+	}
+
+	public ArrayList<Elevator> getElevators() {
+		return elevators;
+	}
+
+	public void setElevators(ArrayList<Elevator> elevators) {
+		this.elevators = elevators;
+	}
+	
 	
 }
--- ElevatorSim/src/ElevatorSim.java
+++ ElevatorSim/src/ElevatorSim.java
@@ -1,8 +1,17 @@
 
+
+
 public class ElevatorSim {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Todo exception handling 
+		if(args.length !=2){
+			System.out.println("2 args expected, found "+ args.length);
+			System.out.println("correct usage is arg[0]: floors, arg[1]:elevators. Ex: '4 3' ");
+		}
+		ElevatorController controller = new ElevatorController(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+		
 
 	}
 
